d7efea8fc940a8a7131ad2f0984fbeeef269cfc5
Ege Gaznepoglu <egaznep@gmail.com>
2021-10-15 02:30:41 +0200

    Add dataset loader

---
+++ src/data/load_dataset.py
@@ -0,0 +1,19 @@
+import torch.utils.data as td
+import torch
+import h5py
+
+class Waveform_dataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_path, file_name=('X.hdf5'), seed=0):
+        self._init_dataset(dataset_path, file_name)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        return torch.from_numpy(self.X[idx])
+
+    def _init_dataset(self, dataset_path, file_name):
+        # features file
+        with h5py.File(dataset_path / file_name, 'r') as X_file:
+            self.X = X_file['sinusoids'][:]
+            self.size = len(self.X)
