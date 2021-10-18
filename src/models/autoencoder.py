b3c2468f02134b8dc8024733c53570ac2bb19d67
Ege Gaznepoglu <egaznep@gmail.com>
2021-10-18 22:18:10 +0200

    Improved autoencoder - Added dropout - Added batch normalization to encoder

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -7,23 +7,34 @@ class AE(nn.Module):
     def __init__(self, num_input=100, num_latent=3, *args, **kwargs):
         super(AE, self).__init__()
         self.encoder = torch.nn.Sequential(
+            torch.nn.Dropout(p=0.2),
             torch.nn.Linear(num_input, 50),
+            torch.nn.BatchNorm1d(50),
             torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
             torch.nn.Linear(50, 25),
+            torch.nn.BatchNorm1d(25),
             torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
             torch.nn.Linear(25, 12),
+            torch.nn.BatchNorm1d(12),
             torch.nn.ReLU(),
-            torch.nn.Linear(12, num_latent)
+            torch.nn.Dropout(p=0.2),
+            torch.nn.Linear(12, num_latent),
+            torch.nn.ReLU(),
         )
 
         self.decoder = torch.nn.Sequential(
             torch.nn.Linear(num_latent, 12), 
             torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
             torch.nn.Linear(12, 25),
             torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
             torch.nn.Linear(25, 50),
             torch.nn.ReLU(),
-            torch.nn.Linear(50, num_input)
+            torch.nn.Dropout(p=0.2),
+            torch.nn.Linear(50, num_input),
         )
 
     def forward(self, x):
