e2f5e6bb31622d3e55d45b8da49648c6d687e320
Ege Gaznepoglu <egaznep@gmail.com>
2021-10-20 22:37:52 +0200

    Fix issues with test_loader having batch_size!=1

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -39,7 +39,7 @@ class Prediction:
             for i, x in enumerate(test_loader):
                 x = x.to(self.device)
                 x_hat = self.model(x)
-                return_list.append(x_hat.cpu().numpy().ravel())
+                return_list.append(x_hat.cpu().numpy())
         return return_list
         
     def to_latent_space(self, test_loader):
@@ -50,8 +50,7 @@ class Prediction:
             for i, x in enumerate(test_loader):
                 x = x.to(self.device)
                 z = self.model.to_latent_space(x)
-                x_hat = self.model.to_waveform(z)
-                return_list.append(z.cpu().numpy().ravel())
+                return_list.append(z.cpu().numpy())
         return return_list
 
     def to_waveform(self, test_loader):
@@ -62,7 +61,7 @@ class Prediction:
             for i, z in enumerate(test_loader):
                 z = z.to(self.device)
                 x_hat = self.model.to_waveform(z)
-                return_list.append(x_hat.cpu().numpy().ravel())
+                return_list.append(x_hat.cpu().numpy())
         return return_list
 
 @click.command()
@@ -96,6 +95,10 @@ def main(data_path, experiment_cfg_path):
 
     inputs = [x.numpy() for x in test_loader]
     results = predictor.predict(test_loader)
+
+    inputs, results = np.asarray(inputs), np.asarray(results)
+    inputs = np.reshape(inputs, (N, -1))
+    results = np.reshape(results, (N, -1))
 
     #automatically visualize if called as the main script
     if __name__ == '__main__':
